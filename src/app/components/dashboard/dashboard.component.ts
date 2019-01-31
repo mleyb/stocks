e9a4a8c7ae97222c4bcfb35194652ada22338cb2
unknown <mleyb@users.noreply.github.com>
2019-01-31 18:39:48 +0000

    Build

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from "@angular/core";
-import { StocksService, Stock } from "../../services/stocks.service";
+import { Component, OnInit } from '@angular/core';
+import { StocksService, Stock } from '../../services/stocks.service';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: "./dashboard.component.html",
-  styleUrls: ["./dashboard.component.css"]
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
   stocks: Array<Stock>;
