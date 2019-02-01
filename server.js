ed668ff1f98b29d2c6690645011ef8b7d8e72a7c
unknown <mleyb@users.noreply.github.com>
2019-02-01 17:52:33 +0000

    Initial commit

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/stocks'));
 
 app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/dist/index.html');
+  res.status(200).sendFile(__dirname + '/dist/stocks/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
